156b15953530894050451f4fae089e5804a48f4e
msyfls123 <msyfls123@gmail.com>
2026-05-31 17:25:25 +0800

    feat: add header nav

--- website/src/components/common/Nav.tsx
+++ website/src/components/common/Nav.tsx
@@ -1,17 +1,27 @@
-import {
-  HoverCard,
-  HoverCardTrigger,
-  HoverCardContent,
-} from '@/components/ui/hover-card'
-import { Button } from '../ui/button'
 
-export const Nav = () => {
-  return <div >
-    <HoverCard defaultOpen>
-      <HoverCardTrigger asChild>
-        <Button>Hover</Button>
-      </HoverCardTrigger>
-      <HoverCardContent>Content</HoverCardContent>
-    </HoverCard>
+import { findRouteCategory, HeaderNavs } from '@/lib/route'
+import { cn } from '@/lib/utils'
+import { useMemo } from 'react'
+
+interface INavProps {
+  routePattern: string
+}
+
+export const Nav = ({ routePattern }: INavProps) => {
+  const foundCategory = useMemo(() => findRouteCategory(routePattern), [routePattern])
+
+  return <div className='relative'>
+    <div className="h-22 border-b-2 border-b-[#c90018] flex justify-between align-middle px-20">
+      <div>LOGO</div>
+      <div className='flex justify-between space-x-6'>
+        {HeaderNavs.map((nav) => (
+          <nav className={cn(' flex justify-center items-center', {
+            'text-[#c90018]': foundCategory === nav.category
+          })}>
+            <span className='text-base leading-none'>{nav.name}</span>
+          </nav>
+        ))}
+      </div>
+    </div>
   </div>
 }
--- website/src/lib/route.ts
+++ website/src/lib/route.ts
@@ -1,25 +1,34 @@
+enum RouteCategory {
+  Home = 'home',
+  Products = 'products',
+  Industries = 'industries',
+  Service = 'service',
+  Advance = 'advance',
+  Profile = 'profile'
+}
+
 export const RoutesMap = {
-  HOME: '/',
-  PRODUCTS: {
+  [RouteCategory.Home]: '/',
+  [RouteCategory.Products]: {
     LIST: '/product/',
     DETAIL: '/product/[id]',
     STARS: '/product/stars'
   },
-  INDUSTRY: {
+  [RouteCategory.Industries]: {
     LIST: '/industry/',
     DETAIL: '/industry/[id]'
   },
-  SERVICE: {
+  [RouteCategory.Service]: {
     SUPPORT: '/service/support',
     CONTACT: '/service/contact',
     PARTNER: '/service/partner',
     RESOURCES: '/service/resources',
   },
-  ADVANCE: {
+  [RouteCategory.Advance]: {
     INNOVATION: '/innovation',
     SUSTAINABLE: '/sustainable',
   },
-  PROFILE: {
+  [RouteCategory.Profile]: {
     ABOUT: '/about',
     MERITS: '/merits',
     NEWS: {
@@ -29,3 +38,47 @@ export const RoutesMap = {
     CAREERS: '/careers'
   }
 }
+
+interface IRouteMap {
+  [k: string]: IRouteMap | string
+}
+
+export const findRouteCategory = (routePattern: string, map: IRouteMap = RoutesMap): string | undefined => {
+  return Object.entries(map).find(([, data]) => {
+    if (typeof data === 'object') {
+      return findRouteCategory(routePattern, data)
+    } else {
+      return routePattern === data
+    }
+  })?.[0]
+}
+
+export const HeaderNavs: Array<{
+  category: RouteCategory;
+  name: string;
+}> = [
+  {
+    category: RouteCategory.Home,
+    name: '首页',
+  },
+  {
+    category: RouteCategory.Products,
+    name: '产品方案',
+  },
+  {
+    category: RouteCategory.Industries,
+    name: '重点行业',
+  },
+  {
+    category: RouteCategory.Service,
+    name: '服务与支持',
+  },
+  {
+    category: RouteCategory.Advance,
+    name: '创新可持续',
+  },
+  {
+    category: RouteCategory.Profile,
+    name: '关于壳盾',
+  },
+]
