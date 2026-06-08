85b5fb51d607a8f13963e4e08db60715cef2a078
msyfls123 <msyfls123@gmail.com>
2026-06-08 20:13:53 +0800

    chore: add nginx config

--- payload/src/payload.config.ts
+++ payload/src/payload.config.ts
@@ -8,7 +8,6 @@ import { en } from '@payloadcms/translations/languages/en'
 import { zh } from '@payloadcms/translations/languages/zh'
 
 import { Users } from './collections/Users'
-import { Media } from './collections/Media'
 import { Products } from './collections/Products'
 import { Industries } from './collections/Industries'
 
@@ -79,18 +78,6 @@ export default buildConfig({
     ProductImages,
     /** files **/
     ResourceFiles,
-    {
-      slug: 'posts',
-      labels: label('示例 Posts'),
-      fields: [
-        {
-          name: 'title',
-          label: 'Title',
-          type: 'text',
-          required: true,
-        },
-      ],
-    },
   ],
   editor: lexicalEditor({
     'features': ({ defaultFeatures }) => ([
@@ -106,11 +93,7 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   async onInit(payload) {
-    const { totalDocs: postsCount } = await payload.count({ collection: 'posts' })
-
-    if (!postsCount) {
-      await payload.create({ collection: 'posts', data: { title: 'Post 1' } })
-    }
+    //
   },
   db: mongooseAdapter({
     url: process.env.DATABASE_URL || '',
